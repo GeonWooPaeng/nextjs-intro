764865b98a42e7a9a469feb4a26b825ef2cb9309
GeonWooPaeng <geonwoopaeng@gmail.com>
2023-02-03 14:14:45 +0900

    feat: fetch data movie data

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from '@/components/NavBar';
+import Seo from '@/components/Seo';
+
+export default function Potato() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+}
